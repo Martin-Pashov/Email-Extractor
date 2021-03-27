f836155e779273ff98fcbbb30a4013f5fbe31a7e
Martin Pashov <80414148+Martin-Pashov@users.noreply.github.com>
2021-03-27 21:31:26 +0200

    Add project files.

---
+++ Email_Extractor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Email_Extractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter some text that contains emails: ");
+            string input = Console.ReadLine();
+
+            string pattern = @"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}";
+            Regex regX = new Regex(pattern, RegexOptions.IgnoreCase);
+            Match fit = regX.Match(input);
+
+            Console.WriteLine();
+            Console.WriteLine("The emails listed below have been extracted ↓");
+            while (fit.Success)
+            {
+                Console.WriteLine($"{fit}");
+
+                fit = fit.NextMatch();
+            }
+        }
+    }
+}
